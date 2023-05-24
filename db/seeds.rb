00352e3823b78d05704e71f080ea3a244eec2b30
celine chader <chader.celine@gmail.com>
2023-05-24 18:12:07 +0200

    exercise air bnb

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,39 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+Flat.destroy_all
+
+Flat.create!(
+  name: "Amazing apartement",
+  address: Faker::Address.full_address,
+  description: Faker::TvShows::NewGirl.quote,
+  price_per_night: 120,
+  number_of_guests: 4
+)
+
+Flat.create!(
+  name: "Big house with garden",
+  address: Faker::Address.full_address,
+  description: Faker::TvShows::NewGirl.quote,
+  price_per_night: 800,
+  number_of_guests: 10
+)
+
+Flat.create!(
+  name: "Nice flat in town",
+  address: Faker::Address.full_address,
+  description: Faker::TvShows::NewGirl.quote,
+  price_per_night: 40,
+  number_of_guests: 2
+)
+
+Flat.create!(
+  name: "Family cottage in the fields",
+  address: Faker::Address.full_address,
+  description: Faker::TvShows::NewGirl.quote,
+  price_per_night: 300,
+  number_of_guests: 6
+)
+
+  puts 'finished seeding'
